1ae0195fe3fb1149e735010d492371ec1e28b1ca
Rachitnarula20 <narularachit22@gmail.com>
2025-01-16 08:14:12 +0530

    array

---
+++ FindMaxConsecutiveOnes.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+int findMaxConsecutiveOnes(vector<int>& nums){
+    int cnt =0,maxi= 0;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (nums[i] == 1)
+        {
+           cnt++;
+        }else{
+            cnt = 0;
+        }
+        maxi = max(maxi, cnt);
+    }
+    return maxi;
+    
+}
--- Remove_duplicates.cpp
+++ Remove_duplicates.cpp
@@ -14,3 +14,6 @@ using namespace std;
     return i+1;
     
  }
+
+
+ 
